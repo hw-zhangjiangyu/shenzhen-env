82ed00077166f3a987f3cc3ccc297da5d6fee4ea
jiangyu.zjy <jiangyu.zjy@alibaba-inc.com>
2019-09-11 16:37:52 +0800

    add update shell

---
+++ cmd/update_version.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+chartName=$1
+releaseVersion=$2
+
+if [ -z $chartName ];
+then
+  echo "chart name is required!"
+  exit 1
+fi
+
+if [ -z $releaseVersion ];
+then
+  echo "release version is required"
+  exit 1
+fi
+
+
+nameAndVersion=`grep ${chartName} ../env/requirements.yaml`
+if [ $? != 0 ];
+then
+  echo "$chartName is not found in current env"
+  exit $?
+fi
+
+# sed "$linenumberc"
+lineNumber=`grep -n ${chartName} ../env/requirements.yaml |awk -F: '{print $1}'`
+if [ -z $lineNumber ]
+then
+  echo "$chartName is not found in current env"
+  exit $?
+fi
+
+sed -i "${lineNumber}c ${chartName}: ${releaseVersion}" ../env/requirements.yaml
+if [ $? != 0 ];
+then
+  echo "Replace release version failed"
+  exit $?
+fi
